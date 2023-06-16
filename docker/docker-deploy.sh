72abdce7fc4b0d55061fb2047dba7eda4368fcc3
chuang.wang <1459731477@qq.com>
2023-06-16 19:15:22 +0800

    add .

--- docker/docker-deploy.sh
+++ docker/docker-deploy.sh
@@ -22,3 +22,46 @@ flush privileges;
 4. or docker netwok connet my_net dockerName//old docker add net
 5.docker inspect my_net //check whether add to docker success
 6.mycli -h mysql156 -uroot -p 123456
+
+#login to server mysql
+mysql -h 150.158.169.158 -P 23306 -uroot -p
+
+#deploy minio
+
+1. docker pull minio/minio
+2. sudo docker run -p 9000:9000 -p 9001:9001 \ 
+    --name psy_minio -e "MINIO_ACCESS_KEY=minioButool"  
+    -e "MINIO_SECRET_KEY=minioButool" \
+    -e "MINIO_ROOT_USER=adminMinio" \
+    -e "MINIO_ROOT_PASSWORD=adminMinio"  \
+    -v /mnt/nfs/dev-aigc-20/data1/chuangwang/server_data/minio/data/:/data \
+    -v /mnt/nfs/dev-aigc-20/data1/chuangwang/server_data/minio/config/:/root/.minio \
+    -d minio/minio server --console-address ':9001' -address ":9000" /data
+
+--console-address 指定客户端端口
+
+3. http://150.158.169.158:9000
+
+#这里的9001端口指的是minio的客户端端口。虽然设置9001，但是我们在访问9000的时候，他也会自动跳到9001。
+#9000端口是minio的服务端端口，我们程序在连接minio的时候，就是通过这个端口来连接的。
+
+# 打开防火墙端口
+firewall-cmd --zone=public --add-port=9000/tcp --permanent
+firewall-cmd --zone=public --add-port=9090/tcp --permanent
+firewall-cmd --reload
+
+ip:9000
+用户名：adminMinio
+密码：adminMinio
+
+
+#deploy redis
+sudo docker run --name redis \
+    -p 6379:6379 -\
+    -restart always \
+    -v /redis/data:/data \
+    -v /redis/conf/redis.conf:/etc/redis/redis.conf \
+    -d redis:latest \
+    redis-server /etc/redis/redis.conf --appendonly yes #redis save data to local
+#reload redis.conf
+redis-cli config rewrite 
---
+++ shell/normal-command.md
@@ -0,0 +1,48 @@
+# Normal Command Collect
+- upload file to server
+```shell
+ sudo scp -i ~/.ssh/chuangwang ./cc-net-test:1.0  chuangwang@42.192.20.81:~
+```
+- find news string in file,and show line number
+```shell
+grep -n news ./cc-cn-hosts.txt.sort
+
+grep news ./cc-cn-hosts.txt.sort > filter_news.txt
+```
+- 100 <line<120
+```shell
+cat cc-cn-hosts.txt.sort |tail -n +100|head -n 20
+```
+- read file line count
+```shell
+wc -l cc-cn-hosts.txt.sort
+```
+- process in linux
+```shell
+ps x /ps a
+```
+- count host name,and sort host by count
+```shell
+time cat cc-cn-hosts.txt|sort|uniq -c|sort -k1 -n -r >cc-cn-hosts.txt.sort
+```
+
+- extract title field from json
+```shell
+sudo cat output/*.json|jq ".title" > clean_titles.txt
+```
+
+extract title and find in json
+```shell
+cat clean_titles.txt |awk '{print $2}' | xargs -i grep '"title": *"{}"' *.json
+```
+
+#delete the title in txt, 
+
+```shell
+sudo cat filter_title.txt | awk -F'"' '{print $2}' | xargs -t -i sudo sed -i '/"title": *"{}"/d' ./text.txt
+```
+
+- change folder owner
+```shell
+sudo chown -R chuangwang:chuangwang /mnt/nfs/dev-aigc-20/data1/chuangwang
+```
